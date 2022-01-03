40ba3f9e36ec82648da646a5103015fb16a6b596
acse-dx121 <dx121@ic.ac.uk>
2022-01-03 18:34:15 +0000

    update

---
+++ tree/124.二叉树中的最大路径和.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=cpp
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (44.64%)
+ * Likes:    1360
+ * Dislikes: 0
+ * Total Accepted:    179.8K
+ * Total Submissions: 402.9K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ * 
+ * 路径和 是路径中各节点值的总和。
+ * 
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int maxLength = INT_MIN;
+public:
+    int myBuildPath(TreeNode* root) {
+        if (root == nullptr) {
+            return INT_MIN;
+        }
+
+        long long leftLength = myBuildPath(root -> left);
+        long long rightLength = myBuildPath(root -> right);
+        long long maxSub = max(leftLength, rightLength);
+        long long maxRoot = max((long long)root -> val, max((long long)root -> val + maxSub, (long long)root -> val + leftLength + rightLength));
+
+        maxRoot = max(maxRoot, maxSub);   
+        if (maxRoot > maxLength) {
+            maxLength = maxRoot;
+        } 
+
+        return maxSub > 0 ? maxSub + root -> val : root -> val;
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        myBuildPath(root);
+        return maxLength;
+    }
+};
+// @lc code=end
+
