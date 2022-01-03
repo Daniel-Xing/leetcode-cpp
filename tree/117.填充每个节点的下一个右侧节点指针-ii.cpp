f6d44540d4bf0cbd8189a5bd40e9f8beab6fd059
xz1220 <3024979463@qq.com>
2022-01-03 17:06:55 +0000

    117

---
+++ tree/117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -0,0 +1,140 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=cpp
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ *
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (60.87%)
+ * Likes:    491
+ * Dislikes: 0
+ * Total Accepted:    104.1K
+ * Total Submissions: 167.2K
+ * Testcase Example:  '[1,2,3,4,5,null,7]'
+ *
+ * 给定一个二叉树
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next
+ * 指针连接），'#' 表示每层的末尾。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数小于 6000
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+
+    Node* connect(Node* root) {
+        Node* leftmost = root;
+        while ( leftmost != nullptr ) {
+            Node* pre = nullptr;
+            Node* next = nullptr;
+            Node* node = leftmost;
+            Node* NextLeftMost = nullptr;
+            while (node != nullptr) {
+                if (node -> left != nullptr) {
+                    if (pre == nullptr) {
+                        pre = node -> left;
+                        NextLeftMost = pre;
+                    }else if (next == nullptr){
+                        next = node -> left;
+                    }
+                }
+
+                if (pre != nullptr && next != nullptr) {
+                    pre -> next = next;
+                    pre = next;
+                    next = nullptr;
+                }
+                
+                if (node -> right != nullptr) {
+                    if (pre == nullptr) {
+                        pre = node -> right;
+                        NextLeftMost = pre;
+                    }else if (next == nullptr){
+                        next = node -> right;
+                    }
+                }
+
+                if (pre != nullptr && next != nullptr) {
+                    pre -> next = next;
+                    pre = next;
+                    next = nullptr;
+                }
+
+                node = node -> next;
+            }
+
+            if (NextLeftMost == leftmost) {
+                break;
+            }else {
+                leftmost = NextLeftMost;
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
+
