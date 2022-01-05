00a05fad88bdfaa067378172f094dff171aced30
acse-dx121 <dx121@ic.ac.uk>
2022-01-05 22:51:53 +0000

    118

---
+++ array/118.杨辉三角.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=cpp
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (73.68%)
+ * Likes:    658
+ * Dislikes: 0
+ * Total Accepted:    247.9K
+ * Total Submissions: 335.9K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ * 
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: numRows = 5
+ * 输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: numRows = 1
+ * 输出: [[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            }
+        }
+        return ret;
+    }
+};
+// @lc code=end
+
