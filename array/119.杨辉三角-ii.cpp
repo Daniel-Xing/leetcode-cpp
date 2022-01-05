347012601a35e4d7e6c2cec2b7560547728fad6e
acse-dx121 <dx121@ic.ac.uk>
2022-01-05 22:53:17 +0000

    119

---
+++ array/119.杨辉三角-ii.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] 杨辉三角 II
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (67.36%)
+ * Likes:    347
+ * Dislikes: 0
+ * Total Accepted:    165.4K
+ * Total Submissions: 245.2K
+ * Testcase Example:  '3'
+ *
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ * 
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: rowIndex = 3
+ * 输出: [1,3,3,1]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: rowIndex = 0
+ * 输出: [1]
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: rowIndex = 1
+ * 输出: [1,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 0 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            row[i] = 1LL * row[i - 1] * (rowIndex - i + 1) / i;
+        }
+        return row;
+    }
+};
+// @lc code=end
+
