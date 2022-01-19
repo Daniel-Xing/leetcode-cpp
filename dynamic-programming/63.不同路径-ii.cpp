6ebe7028ac9d4ca1b0508a9165fb9e30bbf02a54
acse-dx121 <dx121@ic.ac.uk>
2022-01-19 13:21:13 +0000

    63

---
+++ dynamic-programming/63.不同路径-ii.cpp
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ *
+ * https://leetcode-cn.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (39.64%)
+ * Likes:    709
+ * Dislikes: 0
+ * Total Accepted:    208.5K
+ * Total Submissions: 525.8K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * 解释：
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 
+ * obstacleGrid[i][j] 为 0 或 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        auto m = obstacleGrid.size();
+        auto n = obstacleGrid[0].size();
+        std::vector<int> path(n, 0);
+
+        for(int i =0 ; i < m ; i ++) {
+            for (int j =0 ; j < n ; j ++) {
+                if (obstacleGrid[i][j] == 1) {
+                    path[j] = 0;
+                    continue;
+                }
+
+                if (i == 0 && j == 0) {
+                    path[j] = 1;
+                    continue;
+                }
+
+                if (i == 0) {
+                    path[j] = path[j -1];
+                    continue;
+                }
+
+                if (j == 0) {
+                    continue;
+                }
+
+                path[j] += path[j-1];
+            }
+        }
+
+        return path[n-1];
+    }
+};
+// @lc code=end
+
