4c86039a5234d34c328da70ef006bfc5efac90c8
acse-dx121 <dx121@ic.ac.uk>
2022-03-03 23:19:13 +0000

    feature: 124

---
+++ depth-first-search/124.二叉树中的最大路径和.cpp
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=cpp
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (44.22%)
+ * Likes:    1447
+ * Dislikes: 0
+ * Total Accepted:    197.1K
+ * Total Submissions: 440.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ * 
+ * 路径和 是路径中各节点值的总和。
+ * 
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int max_sum = INT_MIN;
+    
+    int dfs(TreeNode* root) {
+        if(root == nullptr) return INT_MIN;
+
+        long left = dfs(root -> left);
+        long right = dfs(root -> right);
+        long return_val = max(long(root -> val), max(left + long(root -> val), right + long(root -> val)));
+
+        long res = max(return_val, left + right + long(root -> val));
+        if(res > max_sum) max_sum = res;
+
+        return return_val;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return max_sum;
+    }
+};
+// @lc code=end
+
