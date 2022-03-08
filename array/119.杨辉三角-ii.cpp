2a25ce18d0f1f3db0c50765aae2c061687dd6abd
acse-dx121 <dx121@ic.ac.uk>
2022-03-08 11:30:37 +0000

    119

--- array/119.杨辉三角-ii.cpp
+++ array/119.杨辉三角-ii.cpp
@@ -62,12 +62,16 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> row(rowIndex + 1);
-        row[0] = 1;
-        for (int i = 1; i <= rowIndex; ++i) {
-            row[i] = 1LL * row[i - 1] * (rowIndex - i + 1) / i;
+        vector<int> ans(rowIndex + 1, 0);
+        ans[0] = 1;
+        for(int i =1 ;i <= rowIndex; i ++) {
+            for(int j = i-1; j >= 1; j --) {
+                ans[j] += ans[j-1];
+            }
+            ans[i] = 1;
         }
-        return row;
+
+        return ans;
     }
 };
 // @lc code=end
